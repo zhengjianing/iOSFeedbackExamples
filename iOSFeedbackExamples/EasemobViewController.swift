d3a1b6b79cf47be928330a45d806fba29030250d
jianing <jianingzheng@gmail.com>
2017-01-10 14:04:47 +0800

    add some empty folders

---
+++ iOSFeedbackExamples/EasemobViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EasemobViewController.swift
+//  iOSFeedbackExamples
+//
+//  Created by Jianing Zheng on 1/10/17.
+//  Copyright © 2017 Jianing Zheng. All rights reserved.
+//
+
+
+// 环信SDK，环信IM功能DEMO
+
+import UIKit
+
+class EasemobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ iOSFeedbackExamples/LeancloudViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LeancloudViewController.swift
+//  iOSFeedbackExamples
+//
+//  Created by Jianing Zheng on 1/10/17.
+//  Copyright © 2017 Jianing Zheng. All rights reserved.
+//
+
+
+// 使用Leancloud的IM，反馈等功能
+
+import UIKit
+
+class LeancloudViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
