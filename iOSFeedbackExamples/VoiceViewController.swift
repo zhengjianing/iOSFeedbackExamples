d1e03a85bee45f54061f6af8d47ad52c63d5fdc5
jianing <jianingzheng@gmail.com>
2017-01-10 13:59:53 +0800

    add comments for pgy

--- iOSFeedbackExamples/VoiceViewController.swift
+++ iOSFeedbackExamples/VoiceViewController.swift
@@ -6,14 +6,30 @@
 //  Copyright © 2017 Jianing Zheng. All rights reserved.
 //
 
+
+//关于蒲公英SDK
+
+// 蒲公英后台APP管理界面地址：https://www.pgyer.com/manager/dashboard/app/11f1a16e8bd916773bf116b1e479590f
+// 蒲公英SDK文档中心：https://www.pgyer.com/doc/view/sdk_ios_guide
+
+//优点:
+//摇一摇、语音反馈等功能都挺好用，后台管理界面也比较清晰
+//工作人员对于用户提出的ticket，会比较及时的回复
+//同时支持iOS和Android，持续更新
+//可以实现APP内自动发现和自动更新新的测试版本
+//文档还可以
+
+//暂时未解决的问题:（2017-01-10）
+//上传：无法通过fastlane从本地上传新版本至蒲公英，只能通过CI配置完成（使用了shenzhen），据说年后会支持
+//摇一摇：start之后就stop不了了，这样就不能只在某个页面使用“摇一摇”功能，因为一旦触发就不能停止了
+//只有OC版本，没有swift版本，必须桥接
+
+
 import UIKit
 
 class VoiceViewController: UIViewController {
 
     @IBOutlet weak var tipsLabel: UILabel!
-
-    // 蒲公英后台APP管理界面地址：https://www.pgyer.com/manager/dashboard/app/11f1a16e8bd916773bf116b1e479590f
-    // 蒲公英SDK文档中心：https://www.pgyer.com/doc/view/sdk_ios_guide
     
     let PgyAppID = "30f52a342978b5a981f21bd580660f8f"
 
